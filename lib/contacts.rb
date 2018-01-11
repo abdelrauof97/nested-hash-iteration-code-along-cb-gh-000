5f346bfc5a22a5ed3b66d409819ece11a990b4c8
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-11 23:24:45 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ require 'pry'
    }
   
 def remove_strawberry(contacts)
-  contacts["Freddy mercury"][:favorite_ice_cream_flavors]
+  contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
   
 end
 binding.pry
